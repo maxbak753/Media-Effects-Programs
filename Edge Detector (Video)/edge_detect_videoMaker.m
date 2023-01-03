6920197f67b9cd0038e104d06666f65d2e8af239
maxbak753 <maxbak753@gmail.com>
2023-01-03 00:45:57 -0500

    Added GUI to Edge Detector & added new videos

--- Edge Detector (Video)/edge_detect_videoMaker.m
+++ Edge Detector (Video)/edge_detect_videoMaker.m
@@ -4,10 +4,29 @@
 
 tic % timer start
 
+% Dialog Box for Input Parameters
+dlg_title = "EDGE DETECTION Video Creator Parameters";
+prompt = {'\bfBlack & White or Color?: \rm0 \rightarrow B&W, 1 \rightarrow RGB'};
+dlg_dims = [1,70];
+definput = {'1'};
+opts.Interpreter = 'tex'; opts.Resize = 'on';
+input_params = inputdlg(prompt,dlg_title,dlg_dims,definput,opts);
+
+% Dialog Box for Video Profile (file type)
+profiles = {'Archival','Motion JPEG AVI','Motion JPEG 2000','MPEG-4', ...
+            'Uncompressed AVI','Indexed AVI','Grayscale AVI'};
+[lst_ind,~] = listdlg('PromptString', {'Select a Video File Format', '(Recommended: MPEG-4 or ','Uncompressed AVI):'}, ...
+                      'SelectionMode', 'single', 'InitialValue', 4, ...
+                      'ListSize',[150,100], ...
+                      'ListString', profiles);
+
+% Video Profile (File Type)
+selected_profile = profiles{lst_ind};
+
 % ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
 % CHOOSE: Black & White or Color? ~ ~ ~ ~ ~
 % 0 -> b&w, 1 -> rgb
-bw_color = 1;
+bw_color = str2double(input_params{1});
 
 if (bw_color == 0)
     bw_color_str = 'bw_';
@@ -20,11 +39,11 @@ end
 % Select original video file
 [vid_name,path] = uigetfile('*.*');
 % Create name for new video file
-new_vid_name = strcat('edgeDetected_',bw_color_str, vid_name, '.mp4');
+new_vid_name = strcat('edgeDetected_',bw_color_str, vid_name);
 
 % Create objects to read and write the video
 reader = VideoReader(strcat(path, vid_name));
-writer = VideoWriter(new_vid_name, 'MPEG-4');
+writer = VideoWriter(new_vid_name, selected_profile);
 % Make frame rates equal
 writer.FrameRate = reader.FrameRate;
 % Open the AVI file for writing
